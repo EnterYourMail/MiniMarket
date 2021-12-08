fb29b9c574603a7e3919f4e3e641e39be474641a
Home <home@local>
2021-12-08 23:45:07 +0400

    test Toolbar and menu

--- app/src/main/java/com/example/minimarket/ui/MainActivity.kt
+++ app/src/main/java/com/example/minimarket/ui/MainActivity.kt
@@ -2,13 +2,41 @@ package com.example.minimarket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.example.minimarket.R
+import com.example.minimarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val MY_LOGS = "MY_LOGS"
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        /*binding.toolbar.setOnMenuItemClickListener { item ->
+            Log.d(MY_LOGS, "MainActivity.setOnMenuItemClickListener")
+            item.onNavDestinationSelected(findNavController(R.id.nav_host))
+                    || super.onOptionsItemSelected(item)
+        }*/
+
+        Log.d(MY_LOGS, "MainActivity.onCreate")
         /*var ed: EditText
         ed.addTextChangedListener(object: TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -23,6 +51,18 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })*/
+    }
+
+ /*   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d("MY_LOGS", "MainActivity.onCreateOptionsMenu")
+        menuInflater.inflate(R.menu.toolbar_start_menu, menu)
+        return true
+    }*/
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(MY_LOGS, "MainActivity.onOptionsItemSelected")
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host))
+                || super.onOptionsItemSelected(item)
     }
 
 
--- app/src/main/java/com/example/minimarket/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/minimarket/ui/list/ListFragment.kt
@@ -2,31 +2,60 @@ package com.example.minimarket.ui.list
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.example.minimarket.R
+import com.example.minimarket.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     companion object {
         fun newInstance() = ListFragment()
     }
-
+    private val MY_LOGS = "MY_LOGS"
+    private var _binding: FragmentListBinding? = null
+    private val binding: FragmentListBinding
+        get() = _binding!!
     private lateinit var viewModel: ListViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        //requireActivity().invalidateOptionsMenu()
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        Log.d("MY_LOGS", "ListFragment.onCreateOptionsMenu")
+        inflater.inflate(R.menu.toolbar_list_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(MY_LOGS, "ListFragment.onOptionsItemSelected")
+        return item.onNavDestinationSelected(findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
